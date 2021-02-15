4a218fc8e47984252d5809d1933844db3b9912dc
Heydar Shams <shamsheidar.79@gmail.com>
2021-02-15 18:28:39 +0330

    active states style

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,15 @@
 const nav = document.getElementById('nav')
+const navList = nav.querySelector('.nav__list')
 const header = document.getElementById('header')
 const navToggleBtn = document.getElementById('nav-toggle')
 
+
 navToggleBtn.addEventListener('click', event => {
+  nav.style.willChange = 'opacity'
+  navList.style.willChange = 'transform'
   header.classList.toggle('nav-open')
+  setTimeout( () => {
+    nav.style.willChange = ''
+    navList.style.willChange = ''
+  }, 250)
 })
