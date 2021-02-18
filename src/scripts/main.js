6bd38d8238897481f7d1087b87052c226f486c75
Heydar Shams <shamsheidar.79@gmail.com>
2021-02-18 14:25:42 +0330

    add new EventListener to nav

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,14 +2,31 @@ const nav = document.getElementById('nav')
 const navList = nav.querySelector('.nav__list')
 const header = document.getElementById('header')
 const navToggleBtn = document.getElementById('nav-toggle')
+let navState = false
 
 
 navToggleBtn.addEventListener('click', event => {
   nav.style.willChange = 'opacity'
   navList.style.willChange = 'transform'
   header.classList.toggle('nav-open')
+  navState = !navState
+
   setTimeout( () => {
     nav.style.willChange = ''
     navList.style.willChange = ''
   }, 250)
+})
+
+addEventListener('click', event => {
+  if(navState && (event.target !== navToggleBtn) && (event.target !== navList)){
+    nav.style.willChange = 'opacity'
+    navList.style.willChange = 'transform'
+    header.classList.remove('nav-open')
+    navState = false
+
+    setTimeout( () => {
+      nav.style.willChange = ''
+      navList.style.willChange = ''
+    }, 250)
+  }
 })
